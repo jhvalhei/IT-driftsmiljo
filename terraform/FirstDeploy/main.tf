3bfec8676aaaf7c8cb2c7a2b541af9d77b7018a2
jhvalhei <jenshermanjh@outlook.com>
2025-02-24 09:16:54 +0100

    testing apply wf

---
+++ terraform/FirstDeploy/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.116.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {   }
+}
+
+resource "azurerm_resource_group" "fd-jh" {
+  name     = "fd-test-jh"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "sa-jhv" {
+  name                     = "jhv1234"
+  resource_group_name      = azurerm_resource_group.fd-jh.name
+  location                 = azurerm_resource_group.fd-jh.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+
+}

d751811a2656ac0f18915dc1a35c166c0dc6d177
Daniel <danith@stud.ntnu.no>
2025-04-09 12:00:40 +0200

    feat: storage module

--- terraform/infrastruktur/main.tf
+++ terraform/infrastruktur/main.tf
@@ -28,59 +28,9 @@ provider "azurerm" {
   subscription_id     = "b03b0d6e-32d0-4c8b-a3df-e5054df8ed86"
 }
 
-
-
-
-
 resource "azurerm_resource_group" "rgstorage" {
   name     = "rg-variablestorage"
   location = var.rg_location_static
-}
-
-
-
-
-# Storage of terraform variables
-resource "azurerm_storage_account" "sa" {
-  name                      = "envstoragegjovik246"
-  resource_group_name       = azurerm_resource_group.rgstorage.name
-  location                  = azurerm_resource_group.rgstorage.location
-  account_tier              = "Standard"
-  account_replication_type  = "LRS"
-  shared_access_key_enabled = false
-}
-
-resource "azurerm_storage_container" "sc" {
-  name                  = "variables"
-  storage_account_name  = azurerm_storage_account.sa.name
-  container_access_type = "private"
-}
-
-resource "azurerm_storage_blob" "ctemplate" {
-  name                   = "containerObj.json"
-  storage_account_name   = azurerm_storage_account.sa.name
-  storage_container_name = azurerm_storage_container.sc.name
-  type                   = "Block"
-  source                 = "${var.rootPath}${var.ctemplatePath}"
-  content_md5            = md5(file("${var.rootPath}${var.ctemplatePath}")) // Forces upload of new file upon changes in file
-}
-
-resource "azurerm_storage_blob" "dbtemplate" {
-  name                   = "databaseObj.json"
-  storage_account_name   = azurerm_storage_account.sa.name
-  storage_container_name = azurerm_storage_container.sc.name
-  type                   = "Block"
-  source                 = "${var.rootPath}${var.dbtemplatePath}"
-  content_md5            = md5(file("${var.rootPath}${var.dbtemplatePath}")) // Forces upload of new file upon changes in file
-}
-
-resource "azurerm_storage_blob" "tfvariables" {
-  name                   = "terraform.tfvars.json"
-  storage_account_name   = azurerm_storage_account.sa.name
-  storage_container_name = azurerm_storage_container.sc.name
-  type                   = "Block"
-  source                 = "${var.rootPath}${var.tfvarsPath}"
-  content_md5            = md5(file("${var.rootPath}${var.tfvarsPath}")) // Forces upload of new file upon changes in file
 }
 
 resource "azurerm_resource_group" "rg_dynamic" {
@@ -93,6 +43,14 @@ resource "azurerm_resource_group" "rg_dynamic" {
 resource "azurerm_resource_group" "rg_static" {
   name     = var.rg_name_static
   location = var.rg_location_static
+}
+
+module "storage" {
+  source = "./modules/storage"
+  rootPath = var.rootPath
+  ctemplatePath = var.ctemplatePath
+  dbtemplatePath = var.dbtemplatePath
+  tfvarsPath = var.tfvarsPath
 }
 
 module "containers" {
---
+++ terraform/infrastruktur/modules/storage/variables.tf
@@ -0,0 +1,22 @@
+variable "rootPath" {
+  description = "Absolute path to infrastructure project"
+  type        = string
+}
+
+variable "ctemplatePath" {
+  description = "Path to container template file"
+  type        = string
+  default     = "/terraform/infrastruktur/templates/containerObj.json"
+}
+
+variable "dbtemplatePath" {
+  description = "Path to database template file"
+  type        = string
+  default     = "/terraform/infrastruktur/templates/databaseObj.json"
+}
+
+variable "tfvarsPath" {
+  description = "Path to .tfvars.json file"
+  type        = string
+  default     = "/terraform/infrastruktur/terraform.tfvars.json"
+}

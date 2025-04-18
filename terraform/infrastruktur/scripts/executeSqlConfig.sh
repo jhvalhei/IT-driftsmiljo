f0a2c600ab84f8d57ef22c678931a5835929d462
Daniel <danith@stud.ntnu.no>
2025-04-18 16:47:21 +0200

    fix: syntax error with quotes

--- terraform/infrastruktur/scripts/executeSqlConfig.sh
+++ terraform/infrastruktur/scripts/executeSqlConfig.sh
@@ -9,8 +9,8 @@ SERVERNAME=$(az postgres flexible-server list -o table | awk 'NR==3 {print $1}')
 KEYVAULTNAME=$(az keyvault list -o table | awk 'NR>2 {print $2}')
 USERSECRET=$(az keyvault secret show --vault-name ${KEYVAULTNAME} -n "${STUDENTDB}secret" -o table | awk 'NR==3 {print $2}')
 
-az postgres flexible-server execute -n "${SERVERNAME}" -u "${USERNAME}" -p "${SECRET}" -d postgres -q "CREATE USER \"${DBUSER}\" WITH PASSWORD \"${USERSECRET}\";" --output table
-az postgres flexible-server execute -n "${SERVERNAME}" -u "${USERNAME}" -p "${SECRET}" -d postgres -q "GRANT ALL PRIVILEGES ON DATABASE \"${STUDENTDB}\" TO \"${DBUSER}\";" --output table
+az postgres flexible-server execute -n "${SERVERNAME}" -u "${USERNAME}" -p "${SECRET}" -d "${STUDENTDB}" -q "CREATE USER \"${DBUSER}\" WITH PASSWORD '${USERSECRET}';" --output table
+az postgres flexible-server execute -n "${SERVERNAME}" -u "${USERNAME}" -p "${SECRET}" -d "${STUDENTDB}" -q "GRANT ALL PRIVILEGES ON DATABASE \"${STUDENTDB}\" TO \"${DBUSER}\";" --output table
 
 # Executes the sql config files
 for z in "${STUDENTDB}"/*DDL*.txt; do
